a35379b3345394e3123d3c498ef64cb1805deb6c
Manish Kumar <manishbgp555@gmail.com>
2026-07-17 09:58:56 +0530

    deploye version

--- llm/gemini.py
+++ llm/gemini.py
@@ -1,11 +1,17 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
 
+google_api_key = st.secrets.get(
+    "GOOGLE_API_KEY",
+    os.getenv("GOOGLE_API_KEY")
+)
+
 llm = ChatGoogleGenerativeAI(
-    model="gemma-4-31b-it",
-    google_api_key=os.getenv("GOOGLE_API_KEY"),
+    model="gemma-4-27b-it",
+    google_api_key=google_api_key,
     temperature=0
 )
